14ea91d4cd44d44c54affccc1c9ab61b52d73f69
Slicepop <46600908+Slicepop@users.noreply.github.com>
2019-11-12 09:58:40 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void hello(){
         System.out.println("Hola Muchachos.");
     }
-
+    public static void homework(){
+        System.out.println("This be my homework.");
+    }
     public static void main(String[] args) {
     hello();
     }
