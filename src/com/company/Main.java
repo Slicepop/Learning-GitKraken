e5f61c7d2890e6193077156a2d82257f76799e56
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-11-12 09:53:13 -0500

    Add Weather() Function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void hello(){
         System.out.println("Hola Muchachos.");
     }
+    public static void weather(){
+        System.out.println("this be the weather");
 
     public static void main(String[] args) {
     hello();
+    weather();
     }
 }
