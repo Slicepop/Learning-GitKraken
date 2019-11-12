d7a94301664b7c9759251dd76f319d3a9ba323bf
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-11-12 09:35:53 -0500

    Add Hello() Function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
 public class Main {
+    public static void hello(){
+        System.out.println("Hola Muchachos.");
+    }
 
     public static void main(String[] args) {
-	// write your code here
+    hello();
     }
 }
